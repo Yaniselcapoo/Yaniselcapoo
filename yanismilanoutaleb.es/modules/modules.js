30685b279591676194066db92f047a3219c70bfe
Yaniselcapoo <134846593+Yaniselcapoo@users.noreply.github.com>
2023-05-28 14:03:05 +0200

    Add files via upload

---
+++ yanismilanoutaleb.es/modules/modules.js
@@ -0,0 +1,9 @@
+crypto('sarten nada', () => {
+    console.log('Modulo', () => {
+        MessagePort.call('Modulos', (FormDataEvent) => {
+            confirm.apply('Modulo 1', () => {
+                alert('Modulo 1. Hecho')
+            })
+        })
+    })
+});
